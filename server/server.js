e52e36b288b585757d89dcf9a64b7d2378e15e5b
Diego Martinez <martinez.diego90@gmail.com>
2023-06-07 20:05:47 +0200

    moodifications

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require("express")
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}, () => console.log('Conectado a MongoDB'));
+
+app.use('/api/calendar', require('./Controllers/CalendarController'));
+app.listen(5000, () => console.log('Servidor iniciado'));
